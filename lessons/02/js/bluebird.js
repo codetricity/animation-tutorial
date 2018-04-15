9509599dfc235fdb0ea687e393a7f0bd2bf3dfcc
Craig Oda <craig@oppkey.com>
2018-04-14 17:48:54 -0700

    added bird

---
+++ lessons/02/js/bluebird.js
@@ -0,0 +1,52 @@
+var game = new Phaser.Game(600, 480);
+
+var GameState = {
+    init: function(){
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.direction = "stop";
+    },
+
+    preload: function(){
+        this.load.spritesheet('girlSheet', 'assets/lesson02/02_bird_75x50x8.png', 75, 50);
+    },
+
+    create: function(){
+        this.girl = this.add.sprite(300, 200, 'girlSheet');
+ 
+        this.girl.anchor.setTo(0.5);
+        this.game.physics.arcade.enable(this.girl);
+        this.walk = this.girl.animations.add('walk', [0, 1, 2, 3, 2, 1], 8, true);
+    },
+
+    update: function(){
+
+        if(this.cursors.right.isDown){
+            this.direction = "right";
+            console.log("direction is right");
+        } else if (this.cursors.left.isDown) {
+            this.direction = "left";
+            console.log("direction is left");
+        } else {
+            this.direction = "stop";
+        }
+
+        if (this.direction == "left") {
+            this.girl.body.velocity.x = -100;
+            this.girl.scale.setTo(1, 1);
+
+            this.girl.animations.play('walk');
+
+        } else if (this.direction == "right") {
+            this.girl.body.velocity.x = 100;
+            this.girl.scale.setTo(-1, 1);
+            this.girl.animations.play('walk');
+        } else if (this.direction == "stop") {
+            this.girl.body.velocity.x = 0;
+            this.girl.frame = 5;
+        }
+    }
+}
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
